5182001619563ee6392e04295529d0c16c3f78ad
thomas-god <thomas.godlewski@icloud.com>
2022-07-25 07:43:48 +0200

    feat: add atom for CalendarDay

---
+++ src/infra/ui/atom/CalendarDay.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  export let status = false;
+  const dispatch = createEventDispatcher();
+  const handleClick = () => {
+    dispatch("calendarDayToggled");
+  };
+</script>
+
+<div on:click={handleClick}>
+  {status ? "X" : ""}
+</div>
