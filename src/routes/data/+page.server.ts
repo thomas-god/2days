caf1c2fb49bba40c68f00ebdd9472bf3f91be16f
thomas-god <thomas.godlewski@icloud.com>
2023-07-15 21:29:45 +0200

    fix(db): use neon directly

--- src/routes/data/+page.server.ts
+++ src/routes/data/+page.server.ts
@@ -1,12 +1,10 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import ws from 'ws';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { pgTable, serial, text, real } from 'drizzle-orm/pg-core';
 import type { InferModel } from 'drizzle-orm';
 import { DATABASE_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
-neonConfig.webSocketConstructor = ws;
 
 const dummyTable = pgTable('playing_with_neon', {
 	id: serial('id').primaryKey(),
@@ -18,8 +16,8 @@ export type TableRow = InferModel<typeof dummyTable>;
 export type NewTableRow = InferModel<typeof dummyTable, 'insert'>;
 
 export const load = (async () => {
-	const pool = new Pool({ connectionString: DATABASE_URL });
-	const db = drizzle(pool);
+	const sql = neon( DATABASE_URL );
+	const db = drizzle(sql);
 	const result = await db.select().from(dummyTable);
 	return { result };
 }) satisfies PageServerLoad;
