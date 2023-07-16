2c723003689c60041da2b7550afd8a9a275b909a
thomas-god <thomas.godlewski@icloud.com>
2023-07-16 09:11:41 +0200

    chore(format): use spaces

--- src/routes/data/+page.server.ts
+++ src/routes/data/+page.server.ts
@@ -5,19 +5,18 @@ import type { InferModel } from 'drizzle-orm';
 import { DATABASE_URL } from '$env/static/private';
 import type { PageServerLoad } from './$types';
 
-
 const dummyTable = pgTable('playing_with_neon', {
-	id: serial('id').primaryKey(),
-	name: text('name').notNull(),
-	value: real('value')
+  id: serial('id').primaryKey(),
+  name: text('name').notNull(),
+  value: real('value')
 });
 
 export type TableRow = InferModel<typeof dummyTable>;
 export type NewTableRow = InferModel<typeof dummyTable, 'insert'>;
 
 export const load = (async () => {
-	const sql = neon( DATABASE_URL );
-	const db = drizzle(sql);
-	const result = await db.select().from(dummyTable);
-	return { result };
+  const sql = neon(DATABASE_URL);
+  const db = drizzle(sql);
+  const result = await db.select().from(dummyTable);
+  return { result };
 }) satisfies PageServerLoad;
--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+  import type { PageData } from './$types';
 
-	export let data: PageData;
+  export let data: PageData;
 </script>
 
 {#if data.result}
-	{#each data.result as row}
-		<p>{row.name}</p>
-	{/each}
+  {#each data.result as row}
+    <p>{row.name}</p>
+  {/each}
 {/if}
