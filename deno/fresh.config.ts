5c3b3aafc674a9cfdd0fa39d3e5bd7038b22f8cb
thomas-god <thomas.godlewski@icloud.com>
2023-10-25 14:38:44 +0200

    feat(deno): handle auth through Github

--- deno/fresh.config.ts
+++ deno/fresh.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "$fresh/server.ts";
 import twindPlugin from "$fresh/plugins/twind.ts";
 import twindConfig from "./twind.config.ts";
+import kvAuth from "./plugins/kv_auth.ts";
 
 export default defineConfig({
-  plugins: [twindPlugin(twindConfig)],
+  plugins: [twindPlugin(twindConfig), kvAuth],
 });
