9da17685c220d92c0c4ca98a7af8ba57db7051fb
thomas-god <thomas.godlewski@icloud.com>
2023-10-28 16:55:21 +0200

    feat(deno): force signin

--- deno/routes/index.tsx
+++ deno/routes/index.tsx
@@ -1,5 +1,17 @@
 import { State } from "../islands/Day.tsx";
 import { HabitSummary } from "../components/HabitSummary.tsx";
+import { getSessionId } from "../plugins/kv_auth.ts";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  async GET(req, ctx) {
+    const session = await getSessionId(req);
+    if (!session) {
+      return Response.redirect("/signin");
+    }
+    return ctx.render();
+  },
+};
 
 export default function Home() {
   const habitName = "Drink water";
